6a65b854a76a1003b5a315f55e55d7e5e1e8cbb9
nandan-varma <nandanvarma.me@gmail.com>
2026-01-06 16:13:53 -0600

    another try at fixing the auth

--- lib/auth-client.ts
+++ lib/auth-client.ts
@@ -3,8 +3,8 @@ import { passkeyClient } from "@better-auth/passkey/client";
 
 export const authClient = createAuthClient({
   // using vercel deployment URL as fallback
-  baseURL: process.env.NEXT_PUBLIC_APP_URL || process.env.VERCEL_URL
-    ? `https://${process.env.VERCEL_URL}`
-    : "http://localhost:3000",
+  baseURL:
+    process.env.NEXT_PUBLIC_APP_URL ||
+    (process.env.VERCEL_URL ? `https://${process.env.VERCEL_URL}` : "http://localhost:3000"),
   plugins: [passkeyClient()],
 });
